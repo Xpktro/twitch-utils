ebd2f9f0dbfab3313ed34c9eb2fdfc85bb780369
Moisés Cachay <xpktro@gmail.com>
2020-10-18 11:53:14 -0500

    removes moment in favor of date-fns

--- deps.ts
+++ deps.ts
@@ -1,3 +1,13 @@
-export { config } from 'https://deno.land/x/dotenv/mod.ts';
-export { Application, Router, RouterContext, RouterMiddleware } from 'https://deno.land/x/oak/mod.ts';
-export { moment } from 'https://deno.land/x/moment/moment.ts';
+export { config } from 'https://deno.land/x/dotenv/mod.ts'
+
+export {
+  Application,
+  Router,
+  RouterContext,
+  RouterMiddleware,
+} from 'https://deno.land/x/oak/mod.ts'
+
+export {
+  intervalToDuration,
+  parseJSON,
+} from 'https://deno.land/x/date_fns@v2.15.0/index.js'
